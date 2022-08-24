3a81f62e6a3c9f199225a5b81a8f6d2bae2d1ed1
dadse <dadse@Jyoti>
2022-08-24 18:45:19 +0530

    creating  base project

---
+++ leetcode-challange/src/main/java/prja/leetcode/challange/App.java
@@ -0,0 +1,12 @@
+package prja.leetcode.challange;
+
+/**
+ * Simple main application
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
